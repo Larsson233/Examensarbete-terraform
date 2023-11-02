13cea6a1d3fe150771e9a4ee8e2e1a0036b24a53
Oscar Larsson <oscarlarsson233@gmail.com>
2023-11-02 22:48:46 +0100

    letsgo

--- terraform-infrastructure.tf
+++ terraform-infrastructure.tf
@@ -2,62 +2,15 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-# resource "aws_instance" "my_instance" {
-#   ami               = "ami-0550c2ee59485be53"
-#   instance_type     = "t3.micro"
-#   key_name          = "AwsKey"
-#   security_groups   = ["security-group-ogge"]
-#   availability_zone = "eu-north-1a"
-#   user_data         = <<-EOF
-#   #!/bin/bash
-#   # Update package lists
-#   sudo yum update -y
-#   sudo amazon-linux-extras enable docker
-#   sudo yum install -y docker
-#   sudo systemctl start docker
-#   sudo systemctl enable docker
-#   sudo usermod -aG docker ec2-user
-#   echo eyJwYXlsb2FkIjoiR0dHNldmWU1qS3ZZRGxlNXE3QWFtc0w4V1VPNkFNNjdlWS8wcDRHajN5ZU9CVVZzK0tQVncxL3FzdGhnRW1Oa0Y4R2Q1RmxVSGVIK0FOM0crVlhvdWNCQW9iaC8veDJZTHlBeFpMb2Q3SlVhbzFpb3JYbVFxcy91eXIxTm8wcXVFMmJ2VDEyUkMycEM2emZ6T0lEWksvS3Y5cVhRYTRqWWxLMHhveWptV3FvdlBnMGFEcmI3LzJWUlg5UEpzOWIyNEYvMUphYXlqWTN3R21PclF6Y05rcU5NYWRVSkFrbG8yYWNDNlN6a3VUc0tsV3JmcVlsbSs0eGpBN1BFQTYyNzVKQjdEYU9kZTR3Znc1emVMNzUrT3h6eCtDVi9paHdQZXBETjVEajQ3NEhhRTFwN1hKdHlzSjBxOEJjN2pseDlLcG5YUjIvWm1qSStmck5VbnV5L0xlcDRsTGJjOXAyL2xvaFBuQ0hWSC84Y0EybG1vS3IvdXExRC9wUjhadW9nRmFVT0JUWTBTRlhNSE1jMmRvdVEwL3ZOQnNNSFg1dTExeHhERHhZTGprS3lyUGI3dVltZTBHUEwycGgxOFU1a1JyMUVRZGtGWU10Wmx0TjlKUnlaZHBQS042WUhNUkxUYWw5ZVpCWTZvSVJra0k4aUpvcmkxeDNPU09uSVY0M3g0Y3RFVFcyQXFzaGUySE5xblB1dUNvbmtnK0lhWUlyV2tTTEY2MEM1QlgyVHNlbm0wMUVmSWVOZVRwUkM1bUFud1dadGxCUW1RZXBKanJ6QUpsTERnaW1iaWVhMTc5NUFBdVRFYXM3T1lCVHY1RUhvZHEyenVtMmlnS2RnOUUrS3prdCtnakJRdHlSSVdlS3lXWDJCZkxjaGVLODh0Y2ZhN0R4WkZBMXZUNklzVWpIaE9xbnA5SFc2Q1ZOUUdPeWgyZzUyMFlhNjROcUVocFQwZmtFRVhsbTBuWjZmQ3M2SEZ5R0NER3cvVTNES2VObW5BOTQzbHJaU0wvN0plNjh5YTdZYTgyd1VUYTR1QkdsWlZBK0djYmFRU2hhR05rOVRKQ0xsOVRnUy9HUWZFR010TlppcUlBdFliVTJQSUROelZia3NhWlJhbzh6WG5TbDRwcUtpMjVJdmdpdDN0Y0xlWmJMVzZJK3FxTmEwM3B4ei9qRGpLQ1U4cXE3ckFOWWRQcWdzZGJkUFVCcG5LR1RORDR0NDkzaGI0R21JWHcxY0l4dFFOdFlNeCsxZFM0ayt0VkRzSVhGd25NZ2xNV2FKc01nMVJLZWk1TktzWmx3OGVTeGhDVzMzaGpYWHFKcXlhSXpPN3luRWFnNWdOdzlPVjNXMUphbFRuL2o2VXJRc1oxQWdmSlQvNjFiQVY1VEJFSlNvaC9KMUYxRkIzUlp0ZlE9PSIsImRhdGFrZXkiOiJBUUVCQUhoK2RTK0JsTnUwTnhuWHdvd2JJTHMxMTV5amQrTE5BWmhCTFpzdW5PeGszQUFBQUg0d2ZBWUpLb1pJaHZjTkFRY0dvRzh3YlFJQkFEQm9CZ2txaGtpRzl3MEJCd0V3SGdZSllJWklBV1VEQkFFdU1CRUVESWFiTzZxalJUdVE4a2UrYmdJQkVJQTdsaWladVBpZUY0YjJURVBoUTZ5VWpSdkNKSms1Vm9Ma0loZER0WDVpT01iK0xZT0NCWkQrelVlUDZETU1xUSs5SzJlNHVCTUdXV0pMaWZzPSIsInZlcnNpb24iOiIyIiwidHlwZSI6IkRBVEFfS0VZIiwiZXhwaXJhdGlvbiI6MTY5ODY1MjEyN30= | docker login -u AWS --password-stdin 338315907544.dkr.ecr.eu-north-1.amazonaws.com
-#   docker pull 338315907544.dkr.ecr.eu-north-1.amazonaws.com/examensarbete:latest
-#   docker run -d -p 80:80 338315907544.dkr.ecr.eu-north-1.amazonaws.com/examensarbete:latest
-#   EOF
-# }
-
-resource "aws_launch_template" "example" {
-
-  name          = "ogges-launch-template"
-  description   = "Example Launch Template for Auto Scaling Group"
-  image_id      = "ami-0550c2ee59485be53"
-  instance_type = "t3.micro"
-  key_name      = "AwsKey"
-
-  iam_instance_profile {
-    name = "NoCredentials" # Replace with your IAM role name or ARN
-  }
-  network_interfaces {
-    device_index    = 0
-    subnet_id       = "subnet-038809d3d73720fb5"
-    security_groups = [aws_security_group.example.id]
-  }
-  user_data = base64encode(<<-EOF
-  #!/bin/bash
-  sudo yum update -y
-  sudo yum install -y docker
-  sudo systemctl start docker
-  sudo systemctl enable docker
-  sudo usermod -aG docker ec2-user
-  sudo aws ecr get-login-password --region eu-north-1 | sudo docker login --username AWS --password-stdin 338315907544.dkr.ecr.eu-north-1.amazonaws.com
-  sudo docker pull 338315907544.dkr.ecr.eu-north-1.amazonaws.com/examensarbete:latest
-  sudo docker run -d -p 80:80 338315907544.dkr.ecr.eu-north-1.amazonaws.com/examensarbete:latest
-  EOF
-  )
-
+resource "aws_ecs_cluster" "my_ecs_cluster" {
+  name = "MyECSCluster"
 }
 
-resource "aws_security_group" "example" {
-  name        = "security-group-ogge"
-  description = "Example security group for port 80 (HTTP)"
-  vpc_id      = "vpc-0fc88ebe86ed9b792"
+# Notera att "DependsOn" inte används i Terraform; Terraform hanterar automatiskt beroenden.
+
+resource "aws_security_group" "my_sec_grp" {
+  name        = "MySecGrp"
+  description = "MySecGrp"
   ingress {
     from_port   = 80
     to_port     = 80
@@ -80,109 +33,158 @@ resource "aws_security_group" "example" {
   }
 }
 
-# Define the target group
-resource "aws_lb_target_group" "example" {
-  name     = "ogges-target-group"
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  family                   = "OggesTaskDef"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE", "EC2"]
+
+  execution_role_arn = "arn:aws:iam::338315907544:role/ECSTaskExecution"
+
+  cpu    = "256"
+  memory = "512"
+  container_definitions = jsonencode([
+    {
+      name  = "oggescontainer"
+      image = "nginx"
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+
+    }
+  ])
+}
+
+resource "aws_ecs_service" "ecs_service" {
+  name            = "OggesECSService"
+  cluster         = aws_ecs_cluster.my_ecs_cluster.id
+  launch_type     = "FARGATE"
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+  desired_count   = 2
+
+  network_configuration {
+    subnets = [
+      "subnet-038809d3d73720fb5",
+      "subnet-0b5058e004fd8e2bb",
+      "subnet-06b09862b947ee15c",
+    ]
+
+    security_groups = [aws_security_group.my_sec_grp.id]
+
+    assign_public_ip = "true"
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+    container_name   = "oggescontainer"
+    container_port   = 80
+  }
+}
+
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "MyTargetGroup"
   port     = 80
   protocol = "HTTP"
   vpc_id   = "vpc-0fc88ebe86ed9b792"
+
+  health_check {
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  target_type = "ip"
 }
 
-# Create the Application Load Balancer (ALB)
-resource "aws_lb" "example" {
-  name               = "example-alb"
-  internal           = false
-  load_balancer_type = "application"
-  subnets            = ["subnet-038809d3d73720fb5", "subnet-0b5058e004fd8e2bb", "subnet-06b09862b947ee15c"] # Specify your subnets
-
-  enable_deletion_protection = false
-
-  security_groups = [aws_security_group.example.id]
-
-  enable_http2 = true
-}
-
-# Attach the target group to the ALB
-resource "aws_lb_listener" "example" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
+resource "aws_lb_listener" "my_listener" {
+  load_balancer_arn = aws_lb.my_load_balancer.arn
   protocol          = "HTTP"
+  port              = 80
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.example.arn
+    target_group_arn = aws_lb_target_group.my_target_group.arn
   }
 }
 
+resource "aws_lb" "my_load_balancer" {
+  name               = "MyLB"
+  load_balancer_type = "application"
+  ip_address_type    = "ipv4"
+  security_groups    = [aws_security_group.my_sec_grp.id]
 
-resource "aws_autoscaling_group" "example" {
-  name              = "ogges-auto-scaling-group"
-  target_group_arns = [aws_lb_target_group.example.arn]
-  launch_template {
-    id      = aws_launch_template.example.id
-    version = "$Latest"
+  subnet_mapping {
+    subnet_id = "subnet-038809d3d73720fb5"
   }
-  vpc_zone_identifier = ["subnet-038809d3d73720fb5", "subnet-0b5058e004fd8e2bb", "subnet-06b09862b947ee15c"] # Specify your subnet IDs here
-  min_size            = 1
-  max_size            = 3
-  desired_capacity    = 2
+
+  subnet_mapping {
+    subnet_id = "subnet-0b5058e004fd8e2bb"
+  }
+
+  subnet_mapping {
+    subnet_id = "subnet-06b09862b947ee15c"
+  }
 }
 
-
-
-
-resource "aws_codebuild_project" "example" {
-  name          = "my-build"
-  build_timeout = 5
-  service_role  = "arn:aws:iam::338315907544:role/oggeaccess"
-
-  source {
-    type      = "CODECOMMIT"
-    location  = "https://git-codecommit.eu-north-1.amazonaws.com/v1/repos/ExamensarbeteCC"
-    buildspec = "buildspec.yml"
-  }
-
-  environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    type         = "LINUX_CONTAINER"
-    image        = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
-    privileged_mode = true
-  }
+resource "aws_codebuild_project" "ogges_build_project" {
+  name         = "TerraformBuildProject"
+  service_role = "arn:aws:iam::338315907544:role/oggeaccess"
 
   artifacts {
     type = "NO_ARTIFACTS"
   }
+
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:7.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
+  }
+
+  source {
+    type     = "CODECOMMIT"
+    location = "https://git-codecommit.eu-north-1.amazonaws.com/v1/repos/ExamensarbeteCC"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      status = "ENABLED"
+    }
+  }
 }
 
-data "aws_codecommit_repository" "example" {
-  repository_name = "ExamensarbeteCC"
-
-}
-
-resource "aws_codepipeline" "example" {
-  name     = "my-pipeline"
-  role_arn = "arn:aws:iam::338315907544:role/oggeaccesspipeline"
+resource "aws_codepipeline" "ogges_pipeline" {
+  name = "TerraformPipeLine"
 
   artifact_store {
-    location = aws_s3_bucket.example.bucket
+    location = "ogges-unika-s3-hink-king"
     type     = "S3"
   }
+
+  role_arn = "arn:aws:iam::338315907544:role/oggeaccesspipeline"
 
   stage {
     name = "Source"
 
     action {
-      name             = "SourceAction"
-      category         = "Source"
-      owner            = "AWS"
-      provider         = "CodeCommit"
-      version          = "1"
-      output_artifacts = ["SourceArtifact"]
+      name     = "SourceAction"
+      category = "Source"
+      owner    = "AWS"
+      version  = "1"
+      provider = "CodeCommit"
 
       configuration = {
-        RepositoryName = "ExamensarbeteCC" # Replace with your CodeCommit repository name
+        RepositoryName = "ExamensarbeteCC"
         BranchName     = "main"
       }
+
+      output_artifacts = ["SourceOutput"]
     }
   }
 
@@ -190,25 +192,44 @@ resource "aws_codepipeline" "example" {
     name = "Build"
 
     action {
-      name             = "BuildAction"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      input_artifacts  = ["SourceArtifact"]
-      output_artifacts = ["BuildArtifact"]
-      version          = "1"
+      name     = "BuildAction"
+      category = "Build"
+      owner    = "AWS"
+      version  = "1"
+      provider = "CodeBuild"
 
       configuration = {
-        ProjectName = aws_codebuild_project.example.name
+        ProjectName = aws_codebuild_project.ogges_build_project.name
       }
+
+      input_artifacts  = ["SourceOutput"]
+      output_artifacts = ["BuildOutput"]
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name     = "DeployAction"
+      category = "Deploy"
+      owner    = "AWS"
+      version  = "1"
+      provider = "ECS"
+
+      configuration = {
+        ClusterName = aws_ecs_cluster.my_ecs_cluster.name
+        ServiceName = aws_ecs_service.ecs_service.name
+        FileName    = "imagedefinitions.json"
+      }
+
+      input_artifacts = ["BuildOutput"]
     }
   }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "ogges-unika-hink-bucket"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "ogges-unika-s3-hink-king"
 }
 
-# sudo aws ecr get-login-password --region eu-north-1 | sudo docker login --username AWS --password-stdin 338315907544.dkr.ecr.eu-north-1.amazonaws.com
-#
-#
+
